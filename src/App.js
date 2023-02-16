ba9ee6c8274407cce52b5d2ea188a4ecb13c4190
andre-bandeli <andre.lbandeli@gmail.com>
2023-02-15 22:55:13 -0300

    feat: add rotas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import './App.css';
+import Login from './pages/login/Login';
+
+import { Route, Routes, useLocation } from "react-router-dom"
+import React, { useEffect } from 'react';
+import Index from './pages/index/Index';
 
 function App() {
+
+  const location = useLocation();
+
+  useEffect(() => {
+    let title = '';
+    switch (location.pathname) {
+      case '/':
+        title = 'Planejador de Manutenção Online';
+        break;
+      case '/login':
+        title = 'Login';
+        break;
+      default:
+        title = 'Planejador de Manutenção Online';
+        break;
+    }
+    document.title = title;
+  }, [location]);
+
   return (
-    <div className="App">
- 
-    </div>
+    <Routes>
+      <Route path="/" element={<Index />} />
+      <Route path="/login" element={<Login />} />
+  </Routes>
   );
 }
 
